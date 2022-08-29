3c8383969406958bec6be7ebe9ed70e5e082d8e5
taelimson <taehly@gmail.com>
2022-08-29 16:58:19 +0900

    test commit

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -39,5 +39,7 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+        
+        //test입니다 커밋
     }
 }
